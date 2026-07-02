eee293205e7ec4b071154f40e2379c0215fa1e31
Joham <jklarenbeek@gmail.com>
2026-07-02 23:47:29 +0200

    Updates

--- assets/OutputPass-BhNB1KJD.js
+++ assets/OutputPass-BhNB1KJD.js
@@ -1,4 +1,4 @@
-import{ar as o,az as n,aA as r,aB as s,aC as l,aD as p,aE as g,aF as f,aG as _,aH as u,aI as h}from"./index-Dg-qqbBo.js";import{P as M,F as m}from"./Pass-BC7IyLjv.js";const i={name:"OutputShader",uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{ao as o,aw as n,ax as r,ay as s,az as l,aA as p,aB as g,aC as f,aD as _,aE as u,aF as h}from"./index-CGZCs-Kk.js";import{P as M,F as m}from"./Pass-jTMCggQ8.js";const i={name:"OutputShader",uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
